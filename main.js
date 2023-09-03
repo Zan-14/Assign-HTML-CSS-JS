57d88aad08dbebb5f15befcab9a31fdcb91424eb
Zan-14 <fauzanjumdinharm@gmail.com>
2023-09-03 14:43:50 +0700

    Replacing CDN with CLI Tailwind

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-/* Features that include JavaScript:
+/* Features that use JavaScript:
 
 1. Hamburger button on the navbar
-2.
+2. Change navbar color when scrolling
+3. Erase a certain text after a certain amount of time
+4. Create a mini quiz
+5. Show date and time (real time)
 */
 
-console.log('testing');
+
+// Change navbar color when scrolling and how to get tailwind class
+window.addEventListener('scroll', function() {
+    const headElm = document.getElementById('nav-bar');
+
+    if (window.scrollY>0) {
+        headElm.classList.add('bg-slate-400','z-[1000]','bg-opacity-50');
+
+    } else {
+        headElm.classList.remove('bg-slate-400','z-[1000]','bg-opacity-50');
+    };
+});
